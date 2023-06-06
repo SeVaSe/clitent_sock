f14b268c7282ffc41309b3c6fd5cacf57a7941a8
sevase <kotaseva8@gmail.com>
2023-06-06 12:25:15 +0300

    обработка подключения клиента к серваку

--- client.py
+++ client.py
@@ -1,10 +1,15 @@
 import socket
 
 # создание сокета клиента
-HOST, PORT = '', 12345
+HOST_SERVER, PORT_SERVER = '', 12345
 sock_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # подключение клиента к серваку
+sock_client.connect((HOST_SERVER, PORT_SERVER))
+print(f'Клиент был подключен к серверу: HOST - [*{HOST_SERVER}*],  PORT - [*{PORT_SERVER}*]')
+
+while True:
+
 
 
 
