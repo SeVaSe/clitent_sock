8e8c94e7361a390cfbf8b27bfe7b78be57dfca42
sevase <kotaseva8@gmail.com>
2023-06-06 12:48:27 +0300

    подпрвил получение ответа от сервера

--- client.py
+++ client.py
@@ -9,8 +9,14 @@ sock_client.connect((HOST_SERVER, PORT_SERVER))
 print(f'Клиент был подключен к серверу: HOST - [*{HOST_SERVER}*],  PORT - [*{PORT_SERVER}*]')
 
 while True:
+    # ввод комманды
     cmd = input('Введите консольную команду: ')
+
+    # отправка
     sock_client.sendall(cmd.encode('utf-8')) # как я понял, sendall - он более надежный в отправке сообщения на сервак
+
+    # получение ответа
+    print(f'Ответ сервера: [- {sock_client.recv(1024).decode("utf-8")} -]')
 
 
 
