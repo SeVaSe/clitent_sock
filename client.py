3191147b1bab8ed976ad3303bb2a11306bd98548
sevase <kotaseva8@gmail.com>
2023-06-06 12:38:31 +0300

    отправка сообщения на сервер

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import socket
 
 # создание сокета клиента
-HOST_SERVER, PORT_SERVER = '', 12345
+HOST_SERVER, PORT_SERVER = 'localhost', 12345
 sock_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # подключение клиента к серваку
@@ -9,7 +9,8 @@ sock_client.connect((HOST_SERVER, PORT_SERVER))
 print(f'Клиент был подключен к серверу: HOST - [*{HOST_SERVER}*],  PORT - [*{PORT_SERVER}*]')
 
 while True:
-
+    cmd = input('Введите консольную команду: ')
+    sock_client.sendall(cmd.encode('utf-8')) # как я понял, sendall - он более надежный в отправке сообщения на сервак
 
 
 
