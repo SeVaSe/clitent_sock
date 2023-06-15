5605d3ab9d0c9fa02c281ef5c87ae3815346aef5
sevase <kotaseva8@gmail.com>
2023-06-16 01:05:24 +0300

    боже даааа, теперь клиент загружает файлы с сервака и не зависает. ура

--- client.py
+++ client.py
@@ -2,92 +2,87 @@ import socket
 import select
 import threading
 
-# функция по окраске текста
+# Функция по окраске текста
 def colorize(text, color_code):
-    if color_code == 'red': # покраска текста в красный (ошибка)
+    if color_code == 'red':  # Покраска текста в красный (ошибка)
         return f'\033[91m{text}\033[0m'
-    elif color_code == 'yellow': # покраска текста в желтый (предупреждение)
+    elif color_code == 'yellow':  # Покраска текста в желтый (предупреждение)
         return f'\033[93m{text}\033[0m'
-    elif color_code == 'green': # покраска текста в зеленый (успешно)
+    elif color_code == 'green':  # Покраска текста в зеленый (успешно)
         return f'\033[92m{text}\033[0m'
     else:
         return f'{text}'
 
-# создание сокета клиента
+
+# Создание сокета клиента
 HOST_SERVER, PORT_SERVER = '192.168.0.103', 12345
 sock_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# подключение клиента к серваку
+# Подключение клиента к серверу
 sock_client.connect((HOST_SERVER, PORT_SERVER))
 print(colorize(f'Клиент был подключен к серверу: HOST - [*{HOST_SERVER}*],  PORT - [*{PORT_SERVER}*]', 'green'), '\n\n')
 
 
 
+# Функция загрузки файла с сервера
+def download_file(file_name):
+    # Отправка команды на сервер для загрузки файла
+    sock_client.sendall(f'download {file_name}'.encode('utf-8'))
+    # Прием файла от сервера
+    with open(file_name, 'wb') as file:
+        while True:
+            readable, _, _ = select.select([sock_client], [], [], 1.0)
+            if readable:
+                file_data = sock_client.recv(1024)
+                if not file_data:
+                    break
+                file.write(file_data)
+            else:
+                break
 
 
 
 
-# функция отправки запросов и получения от них ответа
+# Функция отправки запросов и получения ответа
 def send_message():
-    # функция загрузки файла с сервера
-    def download_file(file_name):
-        # отправка команды на сервер для загрузки файла
-        sock_client.sendall(f'download {file_name}'.encode('utf-8'))
-        # прием файла от сервера
-        file_data = sock_client.recv(1024)
-
-        # сохранение файла на клиенте
-        with open(file_name, 'wb') as file:
-            file.write(file_data)
-
-
     while True:
-        # ввод комманды
+        # Ввод команды
         cmd = input(colorize('Введите консольную команду: ', 'yellow'))
 
-        # отправка
-        sock_client.sendall(cmd.encode('utf-8')) # как я понял, sendall - он более надежный в отправке сообщения на сервак
-
+        # Отправка команды
+        sock_client.sendall(cmd.encode('utf-8'))
 
         if cmd.startswith('download'):
             _, file_name = cmd.split(' ', 1)
             download_file(file_name)
             continue
 
-        elif cmd.startswith('upload'):
-            pass
 
-        # ожидание данных для чтения
-        # символы '_' для игнорирования этих списков, так как они не интересуют в данном контексте
-        # select.select() принимает три списка: readList, writeList, excepList
-        readable, _, _ = select.select([sock_client], [], [], 1.0)  # ожидание в течение 1 секунды
+        readable, _, _ = select.select([sock_client], [], [], 1.0)  # Ожидание в течение 1 секунды
 
         if readable:
-            # получение ответа
+            # Получение ответа
             answer = sock_client.recv(1024).decode("utf-8")
             if answer.strip() == '':
-                print("""Ответ сервера: [- Пусто, это не ошибка! Просто сервер не может вернуть пустое значение, поэтому чтобы 
-                клиент не зависал, пришлось сделать вывод этого сообщения. Например когда вы вводите команду для перехода
-                в другую дирректорию сервер возращает ничего и программа получает соотвественно ничего. Поэтому я решил данную проблему так-]\n""")
+                print("""Ответ сервера: [- Пусто, это не ошибка! Просто сервер не может вернуть пустое значение, поэтому 
+                чтобы клиент не зависал, пришлось сделать вывод этого сообщения. Например, когда вы вводите команду для 
+                перехода в другую директорию сервер возвращает ничего и программа получает соответственно ничего. 
+                Поэтому я решил данную проблему так -]\n""")
             else:
                 print(f'{colorize("Ответ сервера: ", "green")}[- {answer.strip()} -]\n')
         else:
-            print(colorize("""Ответ сервера: [- Пусто, это не ошибка! Просто сервер не может вернуть пустое значение, поэтому чтобы 
-                клиент не зависал, пришлось сделать вывод этого сообщения. Например когда вы вводите команду для перехода
-                в другую дирректорию сервер возращает ничего и программа получает соотвественно ничего. Поэтому я решил данную проблему так -]\n""", 'red'))
+            print(colorize("""Ответ сервера: [- Пусто, это не ошибка! Просто сервер не может вернуть пустое значение, 
+            поэтому чтобы клиент не зависал, пришлось сделать вывод этого сообщения. Например, когда вы вводите команду 
+            для перехода в другую директорию сервер возвращает ничего и программа получает соответственно ничего. 
+            Поэтому я решил данную проблему так -]\n""", 'red'))
 
 
-threard_send = threading.Thread(target=send_message)
-threard_send.start()
+# Запуск функции отправки запросов и получения ответа в отдельном потоке
+thread_send = threading.Thread(target=send_message)
+thread_send.start()
 
+# Ожидание завершения потока
+thread_send.join()
 
-
-
-
-
-
-
-
-
-
-
+# Закрытие соединения с сервером
+sock_client.close()
