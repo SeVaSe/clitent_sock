99a9c71c2fdc5b644d9915a5c5d1fe7edf747130
sevase <kotaseva8@gmail.com>
2023-06-27 01:05:52 +0300

    -_- тестовый будущий серв

--- client_future.py
+++ client_future.py
@@ -18,27 +18,21 @@ def colorize(text, color_code):
         return f'{text}'
 
 
-# Создание сокета клиента
-HOST_SERVER, PORT_SERVER = '192.168.0.103', 12345
-sock_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Подключение клиента к серверу
-sock_client.connect((HOST_SERVER, PORT_SERVER))
-print(colorize(f'Клиент был подключен к серверу: HOST - [*{HOST_SERVER}*],  PORT - [*{PORT_SERVER}*]', 'green'), '')
-print(f'Чтобы открыть справку по командам, введите - {colorize("document", "purple")}\n\n')
-
-
+# ГЛОБАЛЬНЫЕ ПЕРЕМЕННЫЕ
+received_mes_list = []
+lock = threading.Lock()
+cmd_global = ''
 
 
 
 
 # поиск файла и копирование
-def find_copy_file(file_name, destin_path):
+def find_copy_file(file_name, destin_path, sock_client):
     # проверка файла, на то, что есть ли он в текущей дирректории
-    destin_file_now = os.path.join("C:\PYTHON_\_PROJECT_PYTHON\Python_Project_Other\clitent_sock", file_name)
+    destin_file_now = os.path.join("C:/PYTHON_/_PROJECT_PYTHON/Python_Project_Other/clitent_sock", file_name)
     if os.path.exists(destin_file_now):
         print(f'{colorize("Файл уже существует в целевой директории: ", "green")}{colorize(destin_path, "red")}\n')
-        return upload_file(file_name, file_flag=False)
+        return upload_file(file_name, sock_client, file_flag=False)
 
     # поиск файла на пк
     for root, dirs, files in os.walk('/'):
@@ -54,7 +48,7 @@ def find_copy_file(file_name, destin_path):
     return False
 
 # Функция загрузки файла с сервера
-def download_file(file_name):
+def download_file(file_name, sock_client):
     # Отправка команды на сервер для загрузки файла
     sock_client.sendall(f'download {file_name}'.encode('utf-8'))
 
@@ -74,12 +68,12 @@ def download_file(file_name):
 
 
 # функция отправки файла на сервер
-def upload_file(file_name, file_flag=True):
+def upload_file(file_name, sock_client, file_flag=True):
     if file_flag:
         # создаем путь куда сохраним наш файл
         destin_path = os.path.join("C:/PYTHON_/_PROJECT_PYTHON/Python_Project_Other/clitent_sock/cacheCL", file_name)
 
-        if find_copy_file(file_name, destin_path):
+        if find_copy_file(file_name, destin_path, sock_client):
             # чтение нового файла и отправка его содержимого клиенту
             with open(destin_path, 'rb') as file:
                 while True:
@@ -98,68 +92,125 @@ def upload_file(file_name, file_flag=True):
 
 
 
-# Функция отправки запросов и получения ответа
-def send_message():
+##########################################################################################################################################################
+def input_c(sock_client):
+    cmd = input()
+    sock_client.sendall(cmd.encode('utf-8'))
+
+    return cmd
+
+
+# Функция для приема сообщений от сервера
+def receive_message(sock_client):
+    while True:
+        print(colorize('Введите консольную команду: ', 'yellow'))
+
+
+        # answer = sock_client.recv(1024).decode('utf-8')
+        #
+        # if '⚫' in answer:
+        #     print(f'Сообщение от клиента: {answer}')
+
+        readable, _, _ = select.select([sock_client], [], [], 1.0)  # Ожидание в течение 1 секунды
+
+        # Получение ответа
+
+        answer = sock_client.recv(1024).decode('utf-8')
+        if answer.strip() == '':
+            print("""Ответ сервера: [- Пусто, это не ошибка! Просто сервер не может вернуть пустое значение, поэтому 
+                                    чтобы клиент не зависал, пришлось сделать вывод этого сообщения. Например, когда вы вводите команду для 
+                                    перехода в другую директорию сервер возвращает ничего и программа получает соответственно ничего. 
+                                    Поэтому я решил данную проблему так -]\n\n""")
+        elif answer == 'Клиент отключен':
+            print(f'{colorize(answer.upper(), "red")}\n\n')
+            break
+        elif answer == 'Сервер был выключен':
+            print(f'{colorize(answer.upper(), "red")}\n\n')
+            break
+        elif '⚫' in answer:
+            print(f'Сообщение от клиента: {answer}\n\n')
+        elif answer == '':
+            print('Пусто')
+        else:
+            print(f'{colorize("Ответ сервера: ", "green")}[- {answer.strip()} -]\n\n')
+            continue
+
+
+
+
+# ПЕРЕСОБРАЛ ФУНКЦИЮЮЮ СПАААААААААААААААААААААААААААААААААТЬ
+def send_cmd(sock_client):
     while True:
         # Ввод команды
-        cmd = input(colorize('Введите консольную команду: ', 'yellow'))
+
+        cmd = input_c(sock_client)
+
 
         # Отправка команды
-        sock_client.sendall(cmd.encode('utf-8'))
 
 
-        flag_answer_doc = True # флаг для отслежки что использовалась документация
+
+        flag_answer_doc = True  # флаг для отслежки что использовалась документация
         if cmd == 'document':
             print(sock_client.recv(1024).decode('utf-8') + '\n')
             flag_answer_doc = False
-
         elif cmd.startswith('download'):
             _, file_name = cmd.split(' ', 1)
-            download_file(file_name)
+            download_file(file_name, sock_client)
             print(f'Файл был успешно загружен на клиент\n')
             continue
         elif cmd.startswith('upload'):
             _, file_path = cmd.split(' ', 1)
-            upload_file(file_path)
+            upload_file(file_path, sock_client)
             print(f'Файл был успешно отправлен на сервер\n')
             continue
+        elif cmd.startswith('send'):
+            print(f'Сообщение отправленоооооооооооооооо\n')
+            continue
+        elif cmd == 'closeCL':
+            sock_client.close()
+            break
 
 
 
-        readable, _, _ = select.select([sock_client], [], [], 1.0)  # Ожидание в течение 1 секунды
-
-        if readable:
-            # Получение ответа
-            answer = sock_client.recv(1024).decode("utf-8")
-            if answer.strip() == '':
-                print("""Ответ сервера: [- Пусто, это не ошибка! Просто сервер не может вернуть пустое значение, поэтому 
-                чтобы клиент не зависал, пришлось сделать вывод этого сообщения. Например, когда вы вводите команду для 
-                перехода в другую директорию сервер возвращает ничего и программа получает соответственно ничего. 
-                Поэтому я решил данную проблему так -]\n""")
-            elif answer == 'Клиент отключен':
-                print(f'{colorize(answer.upper(), "red")}\n')
-                break
-            elif answer == 'Сервер был выключен':
-                print(f'{colorize(answer.upper(), "red")}\n')
-                break
-            else:
-                if flag_answer_doc:
-                    print(f'{colorize("Ответ сервера: ", "green")}[- {answer.strip()} -]\n')
-                else:
-                    continue
-        else:
-            print(colorize("""Ответ сервера: [- Пусто, это не ошибка! Просто сервер не может вернуть пустое значение, 
-            поэтому чтобы клиент не зависал, пришлось сделать вывод этого сообщения. Например, когда вы вводите команду 
-            для перехода в другую директорию сервер возвращает ничего и программа получает соответственно ничего. 
-            Поэтому я решил данную проблему так -]\n""", 'red'))
 
 
-# Запуск функции отправки запросов и получения ответа в отдельном потоке
-thread_send = threading.Thread(target=send_message)
-thread_send.start()
+
+
+
+
+########################################################################
+
+# Функция отправки запросов и получения ответа
+def send_message():
+    # Создание сокета клиента
+    HOST_SERVER, PORT_SERVER = '192.168.0.103', 12345
+    sock_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Подключение клиента к серверу
+    sock_client.connect((HOST_SERVER, PORT_SERVER))
+    print(colorize(f'Клиент был подключен к серверу: HOST - [*{HOST_SERVER}*],  PORT - [*{PORT_SERVER}*]', 'green'), '')
+    print(f'Чтобы открыть справку по командам, введите - {colorize("document", "purple")}\n\n')
+
+
+
+    # Запуск функции отправки запросов и получения ответа в отдельном потоке
+    thread_receive_mes = threading.Thread(target=receive_message, args=(sock_client,))
+    thread_send_cmd = threading.Thread(target=send_cmd, args=(sock_client,))
+
+    # старт потоков
+    thread_receive_mes.start()
+    thread_send_cmd.start()
+
+    thread_receive_mes.join()
+    thread_send_cmd.join()
+
+
+
+
 
 # Ожидание завершения потока
-thread_send.join()
+
+send_message()
 
 # Закрытие соединения с сервером
-sock_client.close()
